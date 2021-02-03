51e7f9da2d84dc3891ec55eea2fce093fb76410d
Michael Nattinger <michael.nattinger@gmail.com>
2021-02-03 15:30:35 -0600

    macro progress

--- Y1S1/Metrics/Q3/ps2.m
+++ Y1S1/Metrics/Q3/ps2.m
@@ -1,3 +1,11 @@
-clear; close all; clc
+clear; clc; close all
 
-readtable('AE80.dta')
+syms k c kp cp i ip p A palpha pdelta kbar ibar cbar
+f1 = i == A*(kbar/ibar)*k - (cbar/ibar)*c;
+f2 = ip == A*(kbar/ibar)*kp - (cbar/ibar)*cp;
+f3 = kp == (1-pdelta)*kp + pdelta * i;
+f4 = kp*(A*palpha * pdelta * (palpha - 1)/kbar - (1-pdelta)* ibar/kbar)/(A * palpha * pdelta * kbar^(palpha - 1) + (1-pdelta)* ibar/kbar) ...
+    + ip*((1-pdelta)* ibar/kbar)/(A * palpha * pdelta * kbar^(palpha - 1) + (1-pdelta)* ibar/kbar) ...
+    + c + (1-pdelta)*k + (pdelta - 1)*i;
+sol = solve([f1 f2 f3 f4],[kp cp i ip]);
+solcp = simplify(sol.cp);
